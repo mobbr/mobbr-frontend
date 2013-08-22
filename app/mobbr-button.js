68dfaf07ef14aedb503e91d80a077aa64d9e03a3
mobbr1 <mobbr1@240d2095-fe04-4cac-9f82-9710a3587182>
2013-08-22 13:24:03 +0000

    fix mobbr-button.js

--- app/mobbr-button.js
+++ app/mobbr-button.js
@@ -35,10 +35,10 @@ var mobbr = mobbr || (function() {
         mobbrFrame.setAttribute('name', 'mobbr_frame');
         mobbrFrame.setAttribute('frameborder', '0');
         mobbrFrame.style.cssText = 'position:relative;top:-10px;left:0;right:0;bottom:0;opacity:1;filter:alpha(opacity=100); width: 100%; height: 315px; padding:0; margin:0;';
-        //mobbrFrame.src = 'https://mobbr.com/loader';
+        mobbrFrame.src = 'http://www.mobbr.com/lightbox/#/';
 
         mobbrFrame.onload = function() {
-            mobbrFrame.src = 'http://www.mobbr.com/lightbox/#/';
+            //mobbrFrame.src = 'http://www.mobbr.com/lightbox/#/';
             /*if (mobbrFrame.src == 'undefined') {
                 mobbrFrame.src = 'https://mobbr.com/loader';
             }*/
