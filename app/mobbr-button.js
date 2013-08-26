a530a699015d1a4ff7e3d5d56f5b37250c09d213
mobbr1 <mobbr1@240d2095-fe04-4cac-9f82-9710a3587182>
2013-08-26 19:43:32 +0000

    lightbox auto login

--- app/mobbr-button.js
+++ app/mobbr-button.js
@@ -8,8 +8,7 @@
  */
 var mobbr = mobbr || (function() {
     var api_url = 'https://api.mobbr.com';
-    //var ui_url  = 'https://mobbr.com';
-    var ui_url = 'http://mobbr-www.dev.handijk.nl:9000';
+    var ui_url  = 'https://mobbr.com';
     var mobbrDiv = createMobbrDiv();
     var mobbrFrame;
     var buttons_shown = 0;
