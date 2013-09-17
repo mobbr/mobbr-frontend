7e27e2ecaaa60c08dcd772b40fd6dda84bc99cc5
mobbr1 <mobbr1@240d2095-fe04-4cac-9f82-9710a3587182>
2013-09-17 10:34:25 +0000

    prevent double submit

--- app/scripts/controllers/UserSettingsController.js
+++ app/scripts/controllers/UserSettingsController.js
@@ -5,30 +5,42 @@ angular.module('mobbr.controllers').controller('UserSettingsController', functio
     $scope.user = userSession.user;
     $scope.new_email = $scope.user.email;
 
+    $scope.waitingsettings = false;
+    $scope.waitingemail = false;
+    $scope.waitingpassword = false;
     $scope.submitSettings = function () {
+        $scope.waitingsettings = true;
         User.save({user: $scope.user}, function (response) {
+            $scope.waitingsettings = false;
             if (response.result === true) {
                 Msg.setResponseMessage('info', 'Settings saved', response);
             } else {
                 Msg.setResponseMessage('error', 'Could not save settings', response);
             }
         }, function (response) {
+            $scope.waitingsettings = false;
             Msg.setResponseMessage('error', 'Could not save settings', response);
         });
     }
 
     $scope.submitEmail = function (form) {
+        $scope.waitingemail = true;
         User.updateEmail({ new_email: form.email.$modelValue }, function (response) {
+            $scope.waitingemail = false;
             Msg.setResponseMessage('info', 'New email address set', response);
         }, function (response) {
+            $scope.waitingemail = false;
             Msg.setResponseMessage('error', 'Could not change email address', response);
         });
     }
 
     $scope.submitPassword = function (form) {
+        $scope.waitingpassword = true;
         User.updatePassword({ new_password: form.new_password.$modelValue }, function (response) {
+            $scope.waitingpassword = false;
             Msg.setResponseMessage('info', 'New password saved', response);
         }, function (response) {
+            $scope.waitingpassword = false;
             Msg.setResponseMessage('error', 'Could not change password', response);
         });
     }
