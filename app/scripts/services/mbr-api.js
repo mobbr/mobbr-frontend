9201a993f6d70925c9c6c010135bfa1c55f69718
mobbr1 <mobbr1@240d2095-fe04-4cac-9f82-9710a3587182>
2013-09-22 16:03:16 +0000

    fix synced login

--- app/scripts/services/mbr-api.js
+++ app/scripts/services/mbr-api.js
@@ -4,38 +4,8 @@ angular.module('mobbr.services.mbr-api', [
 
         'ngResource'
 
-    ]).factory('HttpLoggedInInterceptor', function($injector, $q){
-        return function( promise ) {
+    ]).factory('User', function ($resource) {
 
-            // convert the returned data using values passed to $http.get()'s config param
-            var resolve = function( value ) {
-
-            };
-
-            var reject = function( response ) {
-
-                var userSession = $injector.get('userSession');
-
-                if(response != null && response.status != null && (response.status === 401 || response.status === 0) && userSession.authenticated){
-
-                    userSession.doLogout();
-
-                }
-
-                return $q.reject(response);
-
-
-            };
-
-            // attach our actions
-            promise.then( resolve, reject );
-
-            // return the original promise
-            return promise;
-        }
-    }).config( function( $httpProvider) {
-        $httpProvider.responseInterceptors.push('HttpLoggedInInterceptor');
-    }).factory('User', function($resource) {
         return $resource(api_url + '/api/user/:action',{},{
             setPassword: {method: 'POST',params : {action:'update_password'},isArray:false},
             register: {method: 'PUT',params :{action:'register_user_send_login_link'}, isArray:false},
@@ -50,7 +20,8 @@ angular.module('mobbr.services.mbr-api', [
             updatePassword: {method: 'POST',params :{action:'update_password'}}
         });
 
-    }).factory('Dashboard', function($resource) {
+    }).factory('Dashboard', function ($resource) {
+
         return $resource(api_url + '/api/dashboard/:action',{},{
             getPayments: {method: 'GET',params: { action: 'type'},isArray:false},
             savePayment: {method: 'PUT',params: {action: 'finalize_payments'}},
@@ -60,7 +31,8 @@ angular.module('mobbr.services.mbr-api', [
             getCurrencies: {method: 'GET',params: { action: 'currencies'},isArray:false},
         });
 
-    }).factory('Domain', function($resource) {
+    }).factory('Domain', function ($resource) {
+
         return $resource(api_url + '/api/domain/:action',{},{
             getPayments: {method: 'GET',params: { action: 'payments'},isArray:false},
             getLocations: {method: 'GET',params: {action: 'locations'},isArray:false},
@@ -68,60 +40,76 @@ angular.module('mobbr.services.mbr-api', [
             isDomainOwner : {method: 'GET',params: {domain:'domain',action: 'is_domain_owner'},isArray:false},
             balances : {method: 'GET',params: {domain:'domain',action: 'balances'},isArray:false},
             info : {method: 'GET',params: {domain:'domain',action: 'domain_info'},isArray:false}
-
         });
-    }).factory('Claim', function($resource) {
+
+    }).factory('Claim', function ($resource) {
+
         return $resource(api_url + '/api/claim/:action',{},{
             paymentDescription: {method: 'GET',params: {action: 'payment_script'},isArray:false},
             checkUrl: {method: 'GET',params: { action: 'check_url'},isArray:false},
             unclaimedPayments: {method: 'GET',params: {action: 'unclaimed_payments'},isArray:false},
             claim: {method: 'POST',params: {action: 'claim_payments'}}
         });
-    }).factory('CreateButton', function($resource) {
+
+    }).factory('CreateButton', function ($resource) {
+
         return $resource(api_url + '/api/script/:action',{},{
-            checkUrl: {method: 'GET',params: {url:'url', action: 'retrieve_script'}},
-            storeJson: {method: 'PUT',params: { action: 'store_script'}},
-            validateJson: {method: 'GET',params: {json:'json', action: 'validate_script'}}
-
+            checkUrl: { method: 'GET',params: { url:'url', action: 'retrieve_script' }},
+            storeJson: { method: 'PUT',params: { action: 'store_script'}},
+            validateJson: { method: 'GET',params: {json:'json', action: 'validate_script' }}
         });
-    }).factory('Balances',function($resource){
+
+    }).factory('Balances', function ($resource) {
+
         return $resource(api_url + '/api/dashboard/:action',{},{
-            balance: {method: 'GET',params: {action: 'balance_totals'},isArray:false},
-            payments: {method: 'GET',params: { action: 'external_payments'},isArray:false}
+            balance: {method: 'GET',params: { action: 'balance_totals' }, isArray:false },
+            payments: {method: 'GET',params: { action: 'external_payments' }, isArray:false }
+        });
 
-        });
-    }).factory('ExchangeRates',function($resource){
+    }).factory('ExchangeRates', function ($resource) {
+
         return $resource(api_url + '/api/util/:action',{},{
-            exchangerates: {method: 'GET',params: {action: 'forex_rates'},isArray:false}
+            exchangerates: { method: 'GET',params: { action: 'forex_rates' }, isArray:false }
         });
-    }).factory('PaymentReciept',function($resource){
-        return $resource(api_url + '/api/payment/:action',{},{
-            getPaymentReciept: {method: 'GET',params: {id:'id',action: 'full_payment_data'},isArray:false}
+
+    }).factory('PaymentReciept', function ($resource) {
+
+        return $resource(api_url + '/api/payment/:action', {}, {
+            getPaymentReciept: {method: 'GET', params: { id: 'id', action: 'full_payment_data'}, isArray: false}
         });
-    }).factory('Url',function($resource){
+
+    }).factory('Url', function ($resource) {
+
         return $resource(api_url + '/api/url/:action',{},{
             getLocations: {method: 'GET',params: {action: 'locations'},isArray:false},
             fullData: {method: 'GET',params: {url:'url',action: 'full_url_data'},isArray:false},
             balances: {method: 'GET',params: {url:'url',action: 'balances'},isArray:false},
             personPayments: {method: 'GET',params: {url:'url',action: 'person_payments'},isArray:false}
         });
-    }).factory('Util', function($resource) {
+
+    }).factory('Util', function ($resource) {
+
         return $resource(api_url + '/api/util/:action',{},{
              currencies: {method: 'GET',params: { action: 'currencies'},isArray:false},
              languages: {method: 'GET',params: { action: 'languages'},isArray:false},
              timezones: {method: 'GET',params: { action: 'timezones'},isArray:false}
         });
-    }).factory('Api', function($resource) {
+
+    }).factory('Api', function ($resource) {
+
         return $resource(api_url + '/api/api/:action',{},{
             list: {method: 'GET',params: { action: 'list'},isArray:false}
         });
+
     }).factory('PaymentNetwork', function ($resource) {
+
         return $resource(api_url + '/api/payment_network/:action', {}, {
             networks: {method: 'GET',params: { action: 'networks'}},
             accountAddresses: {method: 'GET', params: { action: 'account_addresses'}},
             newAccountAddress: {method: 'PUT', params: { action: 'new_account_address'}},
             supportedCurrencies: {method: 'GET', params: { action: 'supported_currencies'}},
             sendPayment: {method: 'POST', params: { action: 'send_payment'}}
-        })
-    });
+        });
+    }
+);
 
