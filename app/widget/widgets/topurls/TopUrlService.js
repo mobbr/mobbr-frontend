98bcfc862e7692b214b6804736658607933a2b33
mobbr1 <mobbr1@240d2095-fe04-4cac-9f82-9710a3587182>
2013-10-16 10:17:39 +0000

    widget

--- app/widget/widgets/topurls/TopUrlService.js
+++ app/widget/widgets/topurls/TopUrlService.js
@@ -5,8 +5,6 @@ angular.module('mobbr.widgets').service('TopUrlService', function ($http) {
 
   var url = 'https://api.mobbr.com/api/global/top_urls';
 
-    console.log('test');
-
   this.urls = function() {
     return $http.get(url).then(function(response) {
       return response.data.result;
