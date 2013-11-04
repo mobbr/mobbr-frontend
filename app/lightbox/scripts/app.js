12f04b4b6451b8c3f93fa386722e6b60cd666c38
mobbr1 <mobbr1@240d2095-fe04-4cac-9f82-9710a3587182>
2013-11-04 11:55:51 +0000

    changed directory structure and lightbox update

--- app/lightbox/scripts/app.js
+++ app/lightbox/scripts/app.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('mobbr.controllers', []);
+angular.module('mobbr.directives', []);
 
-angular.module('mobbr', [
+angular.module('mobbr-lightbox', [
+        'ui.bootstrap',
+        'mobbr.directives',
         'mobbr.controllers',
         'mobbr.services.msg',
         'mobbr.services.mbr-api',
