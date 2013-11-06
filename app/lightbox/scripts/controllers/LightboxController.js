8fe927e3b1e006c50b4a553f0442e1800965d77b
mobbr1 <mobbr1@240d2095-fe04-4cac-9f82-9710a3587182>
2013-11-06 16:25:45 +0000

    lightbox updated

--- app/lightbox/scripts/controllers/LightboxController.js
+++ app/lightbox/scripts/controllers/LightboxController.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('mobbr.controllers')
-    .controller('LightboxController', function ($scope, $location, $timeout, Gateway, Url, userSession, User) {
+    .controller('LightboxController', function ($scope, $location, $timeout, Gateway, Url, userSession, User, Balances) {
 
         var hash,
             error,
             dologin,
             logout,
-            logintimeout;
+            logintimeout,
+            customtimeout;
 
         function check() {
 
@@ -24,11 +25,15 @@ angular.module('mobbr.controllers')
                 $scope.marked = false;
             } else if (hash) {
                 $scope.dologin = false;
-                Url.fullData({ url: /*'https://mobbr.com' 'http://fmt.mobbr.com/3/programmas-documentaires-mogen-ontbreken-innovation-station'*/ document.referrer }, function (response) {
-                    $scope.urlData = response.result;
-                    $scope.noscript = $scope.urlData['participants'] === undefined || $scope.urlData['participants'].length === 0;
-                    $scope.loading = false;
+                Gateway.analyzePayment({ data: window.atob(hash), referrer: document.referrer }, function (response) {
+                    hash = response.result;
+                    Url.fullData({ hash: hash }, function (response) {
+                        $scope.urlData = response.result;
+                        $scope.noscript = $scope.urlData['participants'] === undefined || $scope.urlData['participants'].length === 0;
+                        $scope.loading = false;
+                    });
                 });
+
             } else if (dologin) {
                 $scope.dologin = true;
                 $scope.loading = false;
@@ -41,14 +46,36 @@ angular.module('mobbr.controllers')
                 $scope.json = undefined;
                 $scope.noscript = undefined;
             }
+
+            checkLogin();
+        }
+
+        function checkLogin() {
+            if (userSession.authenticated) {
+                Balances.balance(function (response) {
+                    $scope.userCurrencies = response.result;
+                });
+            }
         }
 
         function register() {
-            Gateway.registerPayment({ referrer: document.referrer || 'http://zaplog.nl', hash: hash }, function (response) {
+            Gateway.registerPayment({ referrer: 'https://mobbr.com/'/*document.referrer*/ || 'http://zaplog.nl', hash: hash }, function (response) {
                 $scope.marked = true;
             }, function (response) {
                 $scope.errormessage = response.data && response.data.message && response.data.message.text;
                 $scope.marked = false;
+            });
+        }
+
+        function perform() {
+            Gateway.performPayment({ referrer: 'https://mobbr.com/'/*document.referrer*/ || 'http://zaplog.nl', hash: hash }, function (response) {
+                $scope.marked = true;
+            }, function (response) {
+                $scope.customerror = response.data.message.text;
+                $timeout.cancel(logintimeout);
+                customtimeout = $timeout(function () {
+                    $scope.customerror = undefined;
+                }, 5000);
             });
         }
 
@@ -62,11 +89,21 @@ angular.module('mobbr.controllers')
             }
         }
 
-        $scope.login = function (data, notifyParent, do_register) {
+        $scope.performPayment = function (data) {
+            if (!userSession.authenticated) {
+                $scope.login(data, false, false, true);
+            } else {
+                perform();
+            }
+        }
+
+        $scope.login = function (data, notifyParent, do_register, do_perform) {
             User.login({ email: data.email.$modelValue, password: data.password.$modelValue }, function (response) {
                 if (response.result != undefined && response.result != null) {
                     userSession.doLogin(response.result, notifyParent);
                     do_register && register();
+                    do_perform && perform();
+                    checkLogin();
                 }
             }, function (response) {
                 $scope.loginerror = true;
