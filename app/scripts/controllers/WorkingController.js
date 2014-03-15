38d6c38887bbf9efb3b85e304e08281ff983e653
Han Dijk <info@handijk.nl>
2014-03-15 22:20:21 +0100

    add working communities

--- app/scripts/controllers/WorkingController.js
+++ app/scripts/controllers/WorkingController.js
@@ -1,5 +1,34 @@
 'use strict';
 
-angular.module('mobbr.controllers').controller('WorkingController', function () {
+angular.module('mobbr.controllers').controller('WorkingController', function ($scope, $filter, Working, ngTableParams, userSession) {
 
+    console.log(userSession);
+
+    Working.urls(function (response) {
+        console.log(response);
+    });
+
+    $scope.personParams = new ngTableParams(
+        {
+            page: 1,
+            count: 0
+        },
+        {
+            counts: [],
+            groupBy: 'url',
+            total: 0,
+            getData: function ($defer, params) {
+                Working.persons(function (response) {
+
+                    var data = response.result,
+                        orderedData = params.sorting() ? $filter('orderBy')(data, $scope.personParams.orderBy()) : data;
+
+                    $scope.personParams.$params.count = data.length;
+                    $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                });
+
+
+            }
+        }
+    );
 });
