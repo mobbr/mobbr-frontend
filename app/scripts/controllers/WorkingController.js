f66e1066aec74bc826a8bb0b7a61217c471d2e91
Han Dijk <info@handijk.nl>
2014-03-24 18:38:55 +0100

    change ngtables to invoicetable

--- app/scripts/controllers/WorkingController.js
+++ app/scripts/controllers/WorkingController.js
@@ -3,27 +3,7 @@
 angular.module('mobbr.controllers').controller('WorkingController', function ($scope, $filter, userSession, Working, Global, ngTableParams, invoiceDialog, pdfGenerator, Msg) {
 
     $scope.Working = Working;
-
-    $scope.suggestedTasks = new ngTableParams(
-        {
-            page: 1,
-            count: 10
-        },
-        {
-            groupBy: 'domain',
-            total: 0,
-            getData: function ($defer, params) {
-                Global.pledges(function (response) {
-
-                    var data = response.result,
-                        orderedData = params.sorting() ? $filter('orderBy')(data, $scope.suggestedTasks.orderBy()) : data;
-
-                    $scope.suggestedTasks.total(data.length);
-                    $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-                });
-            }
-        }
-    );
+    $scope.Global = Global;
 
     $scope.historicTasks = new ngTableParams(
         {
