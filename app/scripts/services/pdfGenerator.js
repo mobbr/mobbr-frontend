a87ceea7db2c03898e3076d6408dff6a1f060da6
Han Dijk <info@handijk.nl>
2014-03-24 09:55:58 +0100

    add invoice prefix and postfix to invoice popup

--- app/scripts/services/pdfGenerator.js
+++ app/scripts/services/pdfGenerator.js
@@ -5,37 +5,42 @@ angular.module('mobbr.services.pdf', []).factory('pdfGenerator', function (userS
         return {
             generate: function (invoice) {
 
+                console.log(invoice);
+
                 var pdf = new jsPDF('p','mm', 'a4', true),
                     data = {
                         contractor: {
                             html: '<ul style="font-family: Helvetica"> \
-                            <li><strong>' + invoice.contractor + '</strong></li> \
-                            <li>' + invoice.firstname + ' ' + invoice.lastname + '</li> \
-                            <li>' + invoice.address + '</li> \
-                            <li>' + invoice.country_of_residence + '</li> \
+                            <li><strong>' + invoice.customer_name + '</strong></li> \
+                            <li>' + invoice.customer_username + '</li> \
+                            <li>' + invoice.customer_address + '</li> \
+                            <li>' + invoice.customer_country + '</li> \
                         </ul>',
+                            width: 70,
                             position: {
-                                x: 160,
+                                x: 145,
                                 y: 10
                             }
                         },
                         contractor_vat: {
                             html: '<ul style="font-family: Helvetica"> \
                                 <li><strong>VAT number</strong></li> \
-                                <li>' + invoice.vat_number + '</li> \
+                                <li>' + invoice.customer_vat_number + '</li> \
                             </ul>',
+                            width: 70,
                             position: {
-                                x: 160,
+                                x: 145,
                                 y: 31
                             }
                         },
                         sourcer: {
                             html: '<ul style="font-family: Helvetica"> \
-                            <li><strong>' + userSession.user.username + '</strong></li> \
-                            <li>' + userSession.user.firstname + ' ' + userSession.user.lastname + '</li> \
-                            <li>' + userSession.user.address + '</li> \
-                            <li>' + userSession.user.country_of_residence + '</li> \
+                            <li><strong>' + invoice.worker_name + '</strong></li> \
+                            <li>' + invoice.worker_username + '</li> \
+                            <li>' + invoice.worker_address + '</li> \
+                            <li>' + invoice.worker_country + '</li> \
                         </ul>',
+                            width: 70,
                             position: {
                                 x: 10,
                                 y: 55
@@ -50,6 +55,7 @@ angular.module('mobbr.services.pdf', []).factory('pdfGenerator', function (userS
                                 <strong>Invoice date</strong> \
                             </li> \
                         </ul>',
+                            width: 70,
                             position: {
                                 x: 10,
                                 y: 120
@@ -58,12 +64,13 @@ angular.module('mobbr.services.pdf', []).factory('pdfGenerator', function (userS
                         infodata: {
                             html: '<ul style="font-family: Helvetica"> \
                             <li> \
-                                <span>' + invoice.id + '</span> \
+                                <span>' + invoice.invoice_id + '</span> \
                             </li> \
                             <li> \
                                 <span>' + invoice.paiddatetime + '</span> \
                             </li> \
                         </ul>',
+                            width: 100,
                             position: {
                                 x: 40,
                                 y: 120
@@ -74,6 +81,7 @@ angular.module('mobbr.services.pdf', []).factory('pdfGenerator', function (userS
                             <h2>' + invoice.description + '</h1> \
                             <h3>' + invoice.uri + '</h2> \
                         </div>',
+                            width: 190,
                             position: {
                                 x: 10,
                                 y: 133
@@ -81,6 +89,7 @@ angular.module('mobbr.services.pdf', []).factory('pdfGenerator', function (userS
                         },
                         roles: {
                             html: invoice.roles && ('<div style="font-family: Helvetica"><strong>Roles</strong></div><div style="font-family: Helvetica">' + invoice.roles + '</div>') || '',
+                            width: 190,
                             position: {
                                 x: 10,
                                 y: 155
@@ -101,8 +110,9 @@ angular.module('mobbr.services.pdf', []).factory('pdfGenerator', function (userS
                                     <strong>Total</strong> \
                                 </li> \
                             </ul>',
+                            width: 100,
                             position: {
-                                x: 135,
+                                x: 145,
                                 y: 205
                             }
                         },
@@ -121,8 +131,9 @@ angular.module('mobbr.services.pdf', []).factory('pdfGenerator', function (userS
                                     <span>' + invoice.currency_iso + ' ' + invoice.amount + '</span> \
                             </li> \
                         </ul>',
+                            width: 70,
                             position: {
-                                x: 160,
+                                x: 170,
                                 y: 205
                             }
                         }
@@ -132,7 +143,7 @@ angular.module('mobbr.services.pdf', []).factory('pdfGenerator', function (userS
 
                 angular.forEach(data, function (element) {
                     pdf.fromHTML(element.html, element.position.x, element.position.y, {
-                        width: 210,
+                        width: element.width,
                         elementHandlers: {
                             '#bypassme': function(element, renderer) {
                                 return true;
