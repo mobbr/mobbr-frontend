9756abc6f45faf8d310dce10c2240cdf48674131
Han Dijk <info@handijk.nl>
2014-03-25 16:31:55 +0100

    refactor a little to solve some issues

--- app/scripts/services/invoiceDialog.js
+++ app/scripts/services/invoiceDialog.js
@@ -2,20 +2,26 @@
 
 angular.module('mobbr.services.invoice', []).factory('invoiceDialog', function ($dialog, userSession, Sourcing, Working) {
 
-        return function (api, method, ids, onSuccess, onError) {
+        return function (method, template, params, onSuccess, onError) {
 
-            var Api = api === 'sourcing' ? Sourcing : Working;
+            //var Api = api === 'sourcing' ? Sourcing : Working;
 
             return $dialog.dialog({
                 backdrop: true,
                 keyboard: true,
                 backdropClick: false,
-                templateUrl: 'views/partials/invoice_popup.html',
+                templateUrl: 'views/partials/' + template + '.html',
                 controller: function ($scope, dialog) {
 
-                    $scope.api = api;
+                    $scope.invoice = {};
 
-                    $scope.invoice = {
+                    angular.forEach(params, function (item, key) {
+                        $scope.invoice[key] = item;
+                    });
+
+                    //$scope.api = api;
+
+                    /*$scope.invoice = {
                         name: userSession.user.companyname || (userSession.user.firstname + ' ' + userSession.user.lastname),
                         address: userSession.user.address,
                         country: userSession.user.country_of_residence,
@@ -24,7 +30,7 @@ angular.module('mobbr.services.invoice', []).factory('invoiceDialog', function (
                         status: userSession.user.companyname && 'enterprise' || 'private',
                         invoice_prefix: userSession.user.invoice_numbering_prefix,
                         invoice_postfix: userSession.user.invoice_numbering_postfix
-                    }
+                    }*/
 
                     $scope.close = function () {
                         dialog.close();
@@ -32,17 +38,9 @@ angular.module('mobbr.services.invoice', []).factory('invoiceDialog', function (
 
                     $scope.confirm = function () {
 
-                        var params = $scope.invoice,
-                            req_params = {},
-                            prefix =  api === 'sourcing' ? 'customer' : 'worker';
-
                         $scope.waiting = true;
-                        angular.forEach(params, function (item, key) {
-                            req_params[prefix + '_' + key] = item;
-                        });
 
-                        req_params.ids = ids;
-                        Api[method](req_params, function (response) {
+                        method($scope.invoice, function (response) {
                             $scope.waiting = false;
                             onSuccess(dialog, response);
                         }, function (response) {
