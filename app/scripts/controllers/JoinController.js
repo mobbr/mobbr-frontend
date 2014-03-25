bfb82942db386a9369fa5299f3bde52fadb14ceb
Han Dijk <info@handijk.nl>
2014-03-25 09:03:29 +0100

    fix join controller

--- app/scripts/controllers/JoinController.js
+++ app/scripts/controllers/JoinController.js
@@ -1,7 +1,25 @@
 'use strict';
 
 
-angular.module('mobbr.controllers').controller('JoinController', function ($scope, apiUrl) {
+angular.module('mobbr.controllers').controller('JoinController', function ($scope, User, Msg) {
 
     $scope.api_url = apiUrl;
+    $scope.waiting = false;
+    $scope.email = $routeParams.email;
+    $scope.register = function () {
+        var user = {'email': $scope.email, 'username': $scope.username, 'password': $scope.password, 'password_control': $scope.password_control};
+        $scope.waiting = true;
+
+        User.register(user, function (response) {
+            Msg.setResponseMessage('info', '', response);
+            $scope.waiting = false;
+            $scope.email = '';
+            $scope.username = '';
+            $scope.password = '';
+            $scope.password_control = '';
+        }, function (response) {
+            Msg.setResponseMessage('error', 'Couldn\'t send information', response);
+            $scope.waiting = false;
+        });
+    }
 });
