65c83e8bae13a7708849cf0cfe3be641ad03ffac
Han Dijk <info@handijk.nl>
2014-04-03 19:08:26 +0200

    tidy up controller directory

--- app/scripts/controllers/RecoverPasswordController.js
+++ app/scripts/controllers/RecoverPasswordController.js
@@ -1,30 +1,24 @@
 'use strict';
 
-
 angular.module('mobbr.controllers').controller('ResetPasswordController', function ($scope, $route, MobbrUser, userSession, Msg) {
 
     $scope.username_or_email = '';
 
-
     $scope.recoverPassword = function () {
-        $scope.waiting = false;
-        if ($scope.username_or_email.length < 1 && $scope.username_or_email.length < 1) {
-            Msg.addWarning('Only Enter username or email to recover password');
-        }
-        else {
-            $scope.waiting = true;
-            MobbrUser.sendLoginLink({'username_or_email': $scope.username_or_email},
-                function (response) {
-                    $scope.waiting = false;
-                    Msg.setResponseMessage('info', 'A email has been sent to you', response);
+        $scope.waiting = true;
+        MobbrUser.sendLoginLink({
+                username_or_email: $scope.username_or_email
+            },
+            function (response) {
+                $scope.waiting = false;
+                Msg.setResponseMessage('info', 'A email has been sent to you', response);
 
-                },
-                function (response) {
-                    $scope.waiting = false;
-                    Msg.setResponseMessage('error', 'Could not recover password', response);
-                });
-
-        }
+            },
+            function (response) {
+                $scope.waiting = false;
+                Msg.setResponseMessage('error', 'Could not recover password', response);
+            }
+        );
     }
 
 });
