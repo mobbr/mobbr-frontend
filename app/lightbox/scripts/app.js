803b8bb062449bf2d498772e23400862f351bb23
Han Dijk <info@handijk.nl>
2014-04-08 10:30:07 +0200

    implement new mobbr api in lightbox

--- app/lightbox/scripts/app.js
+++ app/lightbox/scripts/app.js
@@ -14,37 +14,39 @@ angular.module('mobbr-lightbox', [
         'mobbr.directives',
         'mobbr.controllers',
         'mobbr.services.msg',
-        'mobbr.services.mbr-api',
-        'mobbr.services.storage',
-        'mobbr.services.user',
         'mobbr.filters'
 
-    ]).run([
+    ]).run(function ($http, $rootScope, $route, $location, $window, MobbrApi, MobbrUser) {
 
-        '$http',
-        '$rootScope',
-        'MobbrApi',
-        function ($http, $rootScope, MobbrApi) {
-
-            $rootScope.isTest = function () {
-                return window.location.href.search('test-www.mobbr.com');
-            }
-
-            $rootScope.currenciesMap = {};
-            MobbrApi.forexCurrencies(function (response) {
-                if (response.result != null) {
-                    $rootScope.currenciesMap = response.result;
-                } else if (response.message != null) {
-                    console.log('error loading currencies' + response.error.status);
-                }
-                $rootScope.currenciesMap['MBR'] = 'Mobbr';
+        $rootScope.login = function (email, password) {
+            MobbrUser.passwordLogin({ email: email, password: password }, function () {
+                $location.path('/wallet');
             });
-        }
-    ]).config([
+        };
 
-        '$httpProvider',
-        function ($httpProvider) {
-            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        $rootScope.logout = function () {
+            MobbrUser.logout();
         }
-    ]
+
+        $rootScope.$on('mobbrApi:authchange', function (user) {
+            $route.reload();
+            if ($window.parent && $window.parent.postMessage) {
+                $window.parent.postMessage(user && [ user.username, user.email ].join('|') || 'logout', '*');
+            }
+        });
+
+        $rootScope.isTest = function () {
+            return window.location.href.search('test-www.mobbr.com');
+        }
+
+        $rootScope.currenciesMap = {};
+        MobbrApi.forexCurrencies(function (response) {
+            if (response.result != null) {
+                $rootScope.currenciesMap = response.result;
+            } else if (response.message != null) {
+                console.log('error loading currencies' + response.error.status);
+            }
+            $rootScope.currenciesMap['MBR'] = 'Mobbr';
+        });
+    }
 );
