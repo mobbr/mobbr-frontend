5ca854c431d9e3ea197f5151047a8ea1d8cdd27a
Han Dijk <info@handijk.nl>
2014-04-11 15:28:48 +0200

    remove unneeded dependencies

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,4 +1,3 @@
-(function () {
-  return angular.module("mobbr.config", [])
-    .constant("apiUrl", "http://api.mobbr.dev").constant("environment", "test");
-})();
+'use strict';
+
+angular.module('mobbr.config', []).constant('apiUrl', 'http://api.mobbr.dev').constant('environment', 'test');
