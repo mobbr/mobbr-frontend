0a6377a57d9fa34c90eb74816f07eb394b6c0724
Han Dijk <info@handijk.nl>
2014-04-17 16:12:43 +0200

    bugfix in domaincontroller, call to unclaimed should be to uri/unclaimed not to payments/unclaimed

--- app/scripts/controllers/DomainController.js
+++ app/scripts/controllers/DomainController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mobbr.controllers').controller('DomainController', function ($scope, $routeParams, $window, MobbrDomain, MobbrPerson, MobbrPayment, MobbrBalance, MobbrReferrer) {
+angular.module('mobbr.controllers').controller('DomainController', function ($scope, $routeParams, $window, MobbrDomain, MobbrPerson, MobbrPayment, MobbrBalance, MobbrReferrer, MobbrUri) {
 
     var urlParam = { domain: $window.atob($routeParams.url) };
 
@@ -8,6 +8,6 @@ angular.module('mobbr.controllers').controller('DomainController', function ($sc
     $scope.info = MobbrDomain.info(urlParam);
     $scope.locations = MobbrReferrer.domain(urlParam);
     $scope.persons = MobbrPerson.domain(urlParam);
-    $scope.unclaimed = MobbrPayment.unclaimed(urlParam);
+    $scope.unclaimed = MobbrUri.unclaimed(urlParam);
     $scope.payments = MobbrPayment.domain(urlParam);
 });
