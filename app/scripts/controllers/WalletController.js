05719dbb9114a901b555b0b9105aa33dd0e41fca
Han Dijk <info@handijk.nl>
2014-05-05 11:44:28 +0200

    update mobbrloader with new icon class and fix deposit and withdrawal dialog

--- app/scripts/controllers/WalletController.js
+++ app/scripts/controllers/WalletController.js
@@ -13,17 +13,6 @@ angular.module('mobbr.controllers').controller('WalletController', function ($sc
         reload();
     }
 
-    var querystring = $window.location.search;
-
-    if (querystring) {
-        $window.location.href = $window.location.href.replace(querystring, '') + querystring;
-    }
-
-    function onConfirmDeposit() {
-        $location.search('transactionId', null);
-        reload();
-    }
-
     if ($location.search().transactionId) {
         MobbrXPayment.confirmDeposit({
                 trx_id: $location.search().transactionId
@@ -51,127 +40,25 @@ angular.module('mobbr.controllers').controller('WalletController', function ($sc
     $scope.historicPayments = MobbrPayment.historic();
 
     $scope.openDepositDialog = function () {
-
         $modal.open({
             backdrop: true,
             keyboard: true,
             backdropClick: false,
             templateUrl: 'views/partials/deposit_popup.html',
-            controller: function ($scope, $modalInstance) {
-
-                $scope.networks = {
-                    btc: {
-                        name: 'Bitcoin',
-                        currencies: [ 'BTC' ],
-                        default_currency: 'BTC'
-                    },
-                    iban: {
-                        name: 'IBAN',
-                        currencies: [ 'USD', 'GBP', 'CHF', 'SEK', 'NOK', 'DKK', 'EUR', 'PLN' ],
-                        default_currency: 'EUR'
-                    }
-                };
-
-                $scope.deposit_currency = $scope.networks['iban'].default_currency;
-
-                $scope.confirm = function () {
-                    console.log($scope);
-                    $scope.waiting = true;
-                    MobbrXPayment.prepareDeposit({
-                        currency: $scope.deposit_currency,
-                        amount: $scope.deposit_amount,
-                        note: $scope.deposit_note,
-                        return_url: $window.location.href
-                    }, function (data) {
-                        $scope.waiting = false;
-                        $modalInstance.close(data.result);
-                    }, function (response) {
-                        $scope.waiting = false;
-                    });
-                }
-            }
+            controller: 'DepositController'
         }).result.then(function (response) {
             $window.location.href = response;
         });
     }
 
     $scope.openWithdrawDialog = function () {
-
-        var withdrawDialog = $modal.open({
+        $modal.open({
             backdrop: true,
             keyboard: true,
             backdropClick: false,
             templateUrl: 'views/partials/withdraw_popup.html',
-            controller: function ($scope) {
-
-                $scope.networks = {
-                    btc: {
-                        type: 'OTHER',
-                        name: 'Bitcoin',
-                        currencies: [ 'BTC' ],
-                        default_currency: 'BTC'
-                    },
-                    iban: {
-                        type: 'IBAN',
-                        name: 'IBAN/BIC',
-                        currencies: [ 'USD', 'GBP', 'CHF', 'SEK', 'NOK', 'DKK', 'EUR', 'PLN' ],
-                        default_currency: 'EUR'
-                    },
-                    uk: {
-                        type: 'GB',
-                        name: 'GB',
-                        currencies: [ 'USD', 'GBP', 'CHF', 'SEK', 'NOK', 'DKK', 'EUR', 'PLN' ],
-                        default_currency: 'GBP'
-                    },
-                    us: {
-                        type: 'US',
-                        name: 'US',
-                        currencies: [ 'USD', 'GBP', 'CHF', 'SEK', 'NOK', 'DKK', 'EUR', 'PLN' ],
-                        default_currency: 'USD'
-                    },
-                    ca: {
-                        type: 'CA',
-                        name: 'Canada',
-                        currencies: [ 'USD', 'GBP', 'CHF', 'SEK', 'NOK', 'DKK', 'EUR', 'PLN' ],
-                        default_currency: 'USD'
-                    },
-                    other: {
-                        type: 'OTHER',
-                        name: 'Other',
-                        currencies: [ 'USD', 'GBP', 'CHF', 'SEK', 'NOK', 'DKK', 'EUR', 'PLN' ],
-                        default_currency: 'EUR'
-                    }
-                };
-
-                $scope.deposit_currency = $scope.networks['iban'].default_currency;
-                $scope.network_method = $scope.networks['iban'];
-
-                $scope.$watch('network_method.name', function (oldval, newval) {
-                    $scope.network_method.send = {
-                        currency: $scope.network_method.default_currency,
-                        address: {
-                            type: $scope.network_method.type
-                        }
-                    };
-                }, false);
-
-                $scope.close = function () {
-                    withdrawDialog.close();
-                }
-
-                $scope.confirm = function () {
-                    $scope.waiting = true;
-                    MobbrXPayment.withdraw($scope.network_method.send, function (response) {
-                        $scope.waiting = false;
-                        $scope.network_method.send = {};
-                        withdrawDialog.close();
-                        reload();
-                    }, function (response) {
-                        $scope.waiting = false;
-                    });
-                }
-            }
-        });
+            controller: 'WithdrawController'
+        }).result.then(reload);
     }
 
     reload();
