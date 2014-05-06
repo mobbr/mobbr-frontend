efefc157dea8c67422046f786ae47cc10db1024e
Han Dijk <info@handijk.nl>
2014-05-06 13:35:23 +0200

    remove unneeded services

--- app/scripts/services/authResolver.js
+++ app/scripts/services/authResolver.js
@@ -1,23 +1,21 @@
 'use strict';
 
-angular.module('mobbr.services')
-    .run(function ($rootScope, $state, mobbrMsg, mobbrSession) {
+angular.module('mobbr.services').run(function ($rootScope, $state, mobbrMsg, mobbrSession) {
 
-        $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, error) {
-            if (toState.data && toState.data.authenticated !== mobbrSession.isAuthorized()) {
-                event.preventDefault();
-                mobbrMsg.add({ msg: 'Please login at the account menu' });
-                $state.go(toState.data.redirectTo);
-            }
-        });
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, error) {
+        if (toState.data && toState.data.authenticated !== mobbrSession.isAuthorized()) {
+            event.preventDefault();
+            mobbrMsg.add({ msg: 'Please login at the account menu' });
+            $state.go(toState.data.redirectTo);
+        }
+    });
 
-        $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
-            mobbrMsg.add({ msg: 'Something went wrong trying to open this page', type: 'danger' });
-            if (toState.data && toState.data.redirectTo) {
-                $state.go(toState.data.redirectTo);
-            } else {
-                $state.go('main');
-            }
-        });
-    }
-);
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+        mobbrMsg.add({ msg: 'Something went wrong trying to open this page', type: 'danger' });
+        if (toState.data && toState.data.redirectTo) {
+            $state.go(toState.data.redirectTo);
+        } else {
+            $state.go('main');
+        }
+    });
+});
