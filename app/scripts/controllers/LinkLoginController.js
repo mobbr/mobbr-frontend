016e36b847bbeb80bd07f3d51c19dc4acaa755cf
Han Dijk <info@handijk.nl>
2014-05-13 21:47:04 +0200

    add bitcoin and banktransfer to deposit

--- app/scripts/controllers/LinkLoginController.js
+++ app/scripts/controllers/LinkLoginController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mobbr.controllers').controller('LinkLoginController', function ($scope, $route, $stateParams, MobbrUser) {
+angular.module('mobbr.controllers').controller('LinkLoginController', function ($scope, $stateParams, MobbrUser) {
 
     MobbrUser.linkLogin({ login_token: $stateParams.hash });
 });
