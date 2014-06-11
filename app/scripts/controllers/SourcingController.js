57e1c174ca627d88cfcfc47888b6001778f227f8
Han Dijk <info@handijk.nl>
2014-06-11 16:03:10 +0200

    integrate sourcing and working to invoicing

--- app/scripts/controllers/SourcingController.js
+++ app/scripts/controllers/SourcingController.js
@@ -1,12 +1,6 @@
 'use strict';
 
-angular.module('mobbr.controllers').controller('SourcingController', function ($scope, $state, $location, $rootScope, $modal, pdfGenerator, MobbrInvoice, MobbrPayment, MobbrPerson) {
-
-    $scope.requestableInvoices = MobbrInvoice.requestable();
-    $scope.requestedInvoices = MobbrInvoice.requested();
-    $scope.confirmedInvoices = MobbrInvoice.confirmed();
-    $scope.pledged = MobbrPayment.pledged();
-    $scope.workers = MobbrPerson.paid();
+angular.module('mobbr.controllers').controller('SourcingController', function ($scope, $state, $location, $rootScope, $modal, pdfGenerator, MobbrInvoice, MobbrPayment) {
 
     $scope.openPayment = function (item) {
         $location.path('/payment/' + (item.payment_id || item.id));
@@ -46,6 +40,35 @@ angular.module('mobbr.controllers').controller('SourcingController', function ($
                     $scope.$broadcast('invoicetable', 'sourcing_requested_invoices');
                     $scope.$broadcast('invoicetable', 'working_requested_invoices');
                     $scope.$broadcast('invoicetable', 'sourcing_unrequested_invoices');
+                });
+            }
+        );
+    }
+
+    $scope.confirmInvoices = function (ids, items, table) {
+        $modal.open({
+            backdrop: true,
+            keyboard: true,
+            backdropClick: false,
+            templateUrl: 'views/partials/confirm_invoice_popup.html',
+            controller: function ($scope) {
+                $scope.invoice = {
+                    ids: ids,
+                    worker_name: $rootScope.$mobbrStorage.user.companyname || (($rootScope.$mobbrStorage.user.firstname || '') + ' ' + ($rootScope.$mobbrStorage.user.lastname || '')),
+                    worker_address: $rootScope.$mobbrStorage.user.address,
+                    worker_country: $rootScope.$mobbrStorage.user.country_of_residence,
+                    worker_vat_number: $rootScope.$mobbrStorage.user.vat_number,
+                    worker_vat_rate: $rootScope.$mobbrStorage.user.vat_rate,
+                    worker_status: $rootScope.$mobbrStorage.user.companyname && 'enterprise' || 'private',
+                    worker_invoice_prefix: $rootScope.$mobbrStorage.user.invoice_numbering_prefix,
+                    worker_invoice_postfix: $rootScope.$mobbrStorage.user.invoice_numbering_postfix
+                }
+            }
+        }).result.then(function (result) {
+                MobbrInvoice.confirm(result, function() {
+                    $scope.$broadcast('invoicetable', 'working_requested_invoices');
+                    $scope.$broadcast('invoicetable', 'working_reviewed_invoices');
+                    $scope.$broadcast('invoicetable', 'sourcing_reviewed_invoices');
                 });
             }
         );
