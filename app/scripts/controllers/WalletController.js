4a4365819f42574e7067c51fdef8e1a1c7fc4608
Han Dijk <info@handijk.nl>
2014-06-11 20:10:16 +0200

    put back pledges in the wallet

--- app/scripts/controllers/WalletController.js
+++ app/scripts/controllers/WalletController.js
@@ -3,10 +3,28 @@
 angular.module('mobbr.controllers').controller('WalletController', function ($scope, $modal, $window, $location, MobbrBalance, MobbrXPayment, MobbrPayment) {
 
     $scope.payments = MobbrPayment.get();
+    $scope.pledged = MobbrPayment.pledged();
 
     function reload() {
         $scope.balances = MobbrBalance.user();
         $scope.mutations = MobbrXPayment.get();
+    }
+
+    $scope.removePledgesDialog = function (ids) {
+        return $modal.open({
+            backdrop: true,
+            keyboard: true,
+            backdropClick: false,
+            templateUrl: 'views/partials/remove_pledges_popup.html',
+            controller: function ($scope) {
+                $scope.ids = ids;
+                $scope.confirm = function (result) {
+                    MobbrPayment.unpledge(result, function () {
+                        $scope.$close();
+                    });
+                };
+            }
+        });
     }
 
     $scope.openDepositDialog = function () {
