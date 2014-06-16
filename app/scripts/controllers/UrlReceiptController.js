c61e9bf57f8ccb1a2ddb2d376646c9867401c0b6
Han Dijk <info@handijk.nl>
2014-06-16 11:23:51 +0200

    fix urlpage when not logged in

--- app/scripts/controllers/UrlReceiptController.js
+++ app/scripts/controllers/UrlReceiptController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mobbr.controllers').controller('UrlReceiptController', function ($scope, $stateParams, $rootScope, MobbrBalance, MobbrPayment, MobbrPerson, $window, payment) {
+angular.module('mobbr.controllers').controller('UrlReceiptController', function ($scope, $stateParams, $rootScope, MobbrBalance, MobbrPayment, MobbrPerson, mobbrSession, $window, payment) {
 
     var urlParam = { url: $window.atob($stateParams.url) };
 
@@ -11,7 +11,7 @@ angular.module('mobbr.controllers').controller('UrlReceiptController', function 
     function reload() {
         $scope.balances = MobbrBalance.uri(urlParam);
         $scope.personPayments = MobbrPayment.uri(urlParam);
-        urlParam.base_currency = $rootScope.$mobbrStorage.user.currency_iso || 'EUR';
+        urlParam.base_currency = mobbrSession.isAuthorized() ? $rootScope.$mobbrStorage.user.currency_iso : 'EUR';
         $scope.earners = MobbrPerson.uri_earners(urlParam);
         $scope.payers = MobbrPerson.uri_payers(urlParam);
     }
