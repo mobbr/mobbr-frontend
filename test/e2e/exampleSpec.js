d78b94663075906b3da57ab710109cefc05f7a21
Andre Paap <andre.paap@gmail.com>
2014-07-23 13:47:21 +0200

    added protractor e2e testing

---
+++ test/e2e/exampleSpec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Mobbr e2e example', function () {
+
+
+    var ptor;
+
+    beforeEach(function () {
+        browser.get('http://www.mobbr.dev:9001/#/');
+        ptor = protractor.getInstance();
+        ptor.waitForAngular();
+    });
+
+    it('should display a header text', function () {
+        expect(element(by.css('h1')).getInnerHtml()).toEqual('Turn your website into a crowdsourcing business');
+    });
+
+
+
+});
+
+
