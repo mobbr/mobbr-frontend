6918b4531774a26d9a2485b362c629179a64b826
andre Paap <andre.paap@gmail.com>
2014-08-18 14:33:05 +0200

    #14 payment receipt. Could not test download of invoices yet.

--- app/scripts/controllers/PaymentReceiptController.js
+++ app/scripts/controllers/PaymentReceiptController.js
@@ -1,13 +1,71 @@
-'use strict';
+angular.module('mobbr.controllers').controller('PaymentReceiptController', function ($scope, $stateParams, MobbrPayment, MobbrInvoice, filterFilter, $window) {
+    'use strict';
 
-angular.module('mobbr.controllers').controller('PaymentReceiptController', function ($scope, $state, payment, external) {
-    $scope.payment = payment;
-    $scope.external = external;
-    $scope.close = function () {
-        if ($state.includes('main.payment') || $state.includes('main.x-payment')) {
-            $state.go('main');
-        } else {
-            $scope.$dismiss();
-        }
+    function retrievePayment() {
+        MobbrPayment.info({id: $stateParams.id}).$promise.then(
+            function (response) {
+                if (response.result) {
+                    $scope.payment = response.result;
+                    $scope.recieversAndSenders = [];
+                    angular.forEach($scope.payment.senders, function (sender) {
+                        $scope.recieversAndSenders.push(sender);
+                    });
+                    angular.forEach($scope.payment.receivers, function (reciever) {
+                        $scope.recieversAndSenders.push(reciever);
+                    });
+
+                }
+            }
+        );
     }
+
+    retrievePayment();
+
+    $scope.filterSelectedIds = function (data) {
+        var selected = filterFilter(data, { selected: true});
+        if (selected && selected.length > 0) {
+            var ids = [];
+            angular.forEach(selected, function (elem) {
+                ids.push(elem.unclaim_id);
+            });
+            return ids;
+        }
+        return [];
+    };
+
+
+    $scope.selectAll = {selected: false};
+    $scope.$watch('selectAll.selected', function (newValue) {
+        angular.forEach($scope.recieversAndSenders, function (participant) {
+            if (participant.unclaimed === '1') {
+                participant.selected = newValue;
+            }
+        });
+    });
+
+    $scope.mailSelected = function () {
+
+    };
+
+    $scope.revokeSelected = function () {
+        var selected = $scope.filterSelectedIds($scope.recieversAndSenders);
+        if (selected && selected.length > 0) {
+            MobbrPayment.unclaimShares({ids: selected}).$promise.then(function () {
+                retrievePayment();
+            });
+
+        }
+    };
+
+    $scope.download = function () {
+        var selected = $scope.filterSelectedIds($scope.recieversAndSenders);
+        if (selected && selected.length > 0) {
+            MobbrInvoice.get({unclaimed_ids: selected}).$promise.then(function (data) {
+                var blob = new Blob([data], {});
+                var objectUrl = URL.createObjectURL(blob);
+                $window.open(objectUrl);
+            });
+
+        }
+    };
 });
