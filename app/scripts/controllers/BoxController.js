3696d9bf5a821af4d333f44cc5f8cb98d95f4008
Han Dijk <info@handijk.nl>
2014-09-09 22:41:54 +0200

    #32, #13, creating tags section for crowds and tasks

--- app/scripts/controllers/BoxController.js
+++ app/scripts/controllers/BoxController.js
@@ -1,17 +1,44 @@
 /* global purl */
-angular.module('mobbr.controllers').controller('BoxController', function ($scope, MobbrKeywords) {
+angular.module('mobbr.controllers').controller('BoxController', function ($scope, $rootScope, $state, $window, mobbrSession, MobbrKeywords) {
     'use strict';
 
-    function refreshTags() { $scope.tagsChanged = $scope.tags && $scope.tags.result && $scope.tags.result.length !== $scope.filteredTags.length;
+    function refreshTags() {
+        $scope.tagsChanged = $scope.tags && $scope.tags.result && angular.equals($scope.tags.result, $scope.filteredTags.length);
         $scope.$broadcast('update-tags');
     }
 
-    $scope.resetTags = function () {
-        $scope.filteredTags = [];
-        angular.forEach($scope.tags.result, function (keyword) {
-            $scope.filteredTags.push(keyword.keyword);
+    function queryPeople(url) {
+
+        var api, params;
+
+        if ($state.includes('box.crowds') && url && url !== $scope.activeQuery) {
+            api = MobbrKeywords.uri;
+            params = {
+                language: $scope.filter_language,
+                url: url
+            };
+        } else if (mobbrSession.isAuthorized()) {
+            api = MobbrKeywords.person;
+            params = {
+                language: $scope.filter_language,
+                username: $rootScope.$mobbrStorage.user.username
+            };
+        } else {
+            api = MobbrKeywords.get;
+            params = {
+                language: $scope.filter_language
+            };
+        }
+
+        $scope.tags = api(params);
+        $scope.tags.$promise.then(function () {
+            $scope.filteredTags = [];
+            angular.forEach($scope.tags.result, function (keyword) {
+                $scope.filteredTags.push(keyword.keyword);
+            });
+            refreshTags();
         });
-    };
+    }
 
     $scope.addTag = function () {
         if ($scope.form.newTag && $scope.form.newTag.length > 0) {
@@ -35,10 +62,31 @@ angular.module('mobbr.controllers').controller('BoxController', function ($scope
         keywords.splice(keywords.indexOf(tag), 1);
     };
 
-    $scope.$on('set-tags', function (event, method, params) {
+    $scope.$on('$stateChangeStart', function (event, toState, toParams) {
+
+        if (toState.name === 'box.crowds' || toState.name === 'box.tasks') {
+            queryPeople(toParams.task && $window.atob(toParams.task) || undefined);
+        }
+
+        if (toState.name.indexOf('box.crowds.task') === 0) {
+            queryPeople(toParams.task && $window.atob(toParams.task) || undefined);
+            //console.log('zettum');
+            //$scope.$emit('set-query');
+            //$scope.$emit('set-active-query');
+        }
+    });
+
+    /*$rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+        $scope.tag = toParams.tag;
+        if ($state.includes('box.tasks')) {
+            queryFilter();
+        }
+    });*/
+
+    /*$scope.$on('set-tags', function (event, method, params) {
         $scope.tags = MobbrKeywords[method](params);
         $scope.tags.$promise.then($scope.resetTags);
-    });
+    });*/
 
     $scope.$on('set-query', function (event, query) {
         $scope.query = query;
@@ -48,15 +96,13 @@ angular.module('mobbr.controllers').controller('BoxController', function ($scope
         $scope.activeQuery = activeQuery;
     });
 
+    $scope.$on('filter-update', refreshTags);
+
     $scope.$watch('filteredTags', function (newValue, oldValue) {
         if (newValue !== undefined) {
             refreshTags();
         }
     }, true);
 
-    $scope.$watch('filter_language', function (newValue, oldValue) {
-        if (newValue !== undefined) {
-            refreshTags();
-        }
-    });
+    queryPeople($state.params.task && $window.atob($state.params.task) || undefined);
 });
--- app/scripts/controllers/TasksController.js
+++ app/scripts/controllers/TasksController.js
@@ -1,6 +1,12 @@
 'use strict';
 
-angular.module('mobbr.controllers').controller('TasksController', function ($scope, $rootScope, $state, $stateParams, MobbrUri, MobbrKeywords, MobbrApi) {
+angular.module('mobbr.controllers').controller('TasksController', function ($scope, $rootScope, $state, $stateParams, mobbrSession, MobbrUri, MobbrKeywords, MobbrApi) {
+
+    function filterUpdate(newValue, oldValue) {
+        if (newValue !== oldValue) {
+            $scope.$emit('filter-update');
+        }
+    }
 
     $scope.order_options = [
         {
@@ -33,28 +39,17 @@ angular.module('mobbr.controllers').controller('TasksController', function ($sco
         }
     ];
 
-    function queryFilter() {
+    $scope.$on('update-tags', function () {
         $scope.tasks = MobbrUri.get({
-            language: $scope.filter_language || null,
-            keywords: $scope.tag || null,
+            language: $scope.filter_language,
+            keywords: $scope.filteredTags,
             username: $scope.user_tasks && $rootScope.$mobbrStorage.user && $rootScope.$mobbrStorage.user.username || null
         });
-
-        $scope.keywords = MobbrKeywords.get({
-            language: $scope.filter_language || null,
-            related_to: $scope.tag || null
-        });
-    }
-
-    $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
-        $scope.tag = toParams.tag;
-        if ($state.includes('box.tasks')) {
-            queryFilter();
-        }
     });
 
-    $scope.$watch('[ filter_language, filter_order, user_tasks ]', queryFilter, true);
-    $scope.languages = MobbrApi.languages({ include_unused: false });
-    $scope.tag = $state.params.tag;
     $scope.filter_order = $scope.order_options[0].code;
+
+    $scope.$watch('filter_language', filterUpdate);
+    $scope.$watch('filter_order', filterUpdate);
+    $scope.$watch('user_tasks', filterUpdate);
 });
