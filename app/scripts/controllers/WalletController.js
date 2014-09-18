a550fd60a765f69a2f7ce2d7f7e70abcdd400e02
Han Dijk <info@handijk.nl>
2014-09-18 19:51:37 +0200

    show addresses in bitcoin deposit, references #30

--- app/scripts/controllers/WalletController.js
+++ app/scripts/controllers/WalletController.js
@@ -1,6 +1,14 @@
 'use strict';
 
-angular.module('mobbr.controllers').controller('WalletController', function ($scope, $modal, MobbrXPayment, balance) {
+angular.module('mobbr.controllers').controller('WalletController', function ($scope, $modal, MobbrXPayment, balance, supportedCurrencies) {
 
     $scope.dashboard = balance;
+
+    $scope.supportedCurrencies = supportedCurrencies;
+
+    $scope.addBitcoinAddress = function (currency) {
+        MobbrXPayment.newAccountAddress({ currency: currency }, function () {
+            supportedCurrencies.$supportedCurrencies();
+        });
+    }
 });
