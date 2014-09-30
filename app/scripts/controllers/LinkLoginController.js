9b54c44d2fdd34409e2ce1726a1b8511209ac620
Han Dijk <info@handijk.nl>
2014-09-30 12:54:43 +0200

    confirm email and confirm email id are now functional, #99, #100

--- app/scripts/controllers/LinkLoginController.js
+++ app/scripts/controllers/LinkLoginController.js
@@ -3,7 +3,7 @@
 angular.module('mobbr.controllers').controller('LinkLoginController', function ($scope, $stateParams, $state, MobbrUser) {
 
     MobbrUser.linkLogin({ login_token: $stateParams.hash }, function () {
-        $state.go('wallet');
+        $state.go('updates');
     }, function () {
         $state.go('main');
     });
--- app/scripts/controllers/UpdateEmailController.js
+++ app/scripts/controllers/UpdateEmailController.js
@@ -1,12 +1,17 @@
 'use strict';
 
-angular.module('mobbr.controllers').controller('UpdateEmailController', function ($scope, $location, $stateParams, MobbrUser) {
+angular.module('mobbr.controllers').controller('UpdateEmailController', function ($scope, $state, mobbrSession, MobbrUser) {
 
-    MobbrUser.confirmEmail({
-            update_token: $stateParams.hash
-        },
-        function (response) {
-            $location.path('/settings');
-        }
-    );
+    var is_id = $state.current.name === 'id',
+        params = is_id ? { confirm_token: $state.params.hash } : { update_token: $state.params.hash };
+
+    function gotoIds() {
+        $state.go(is_id ? 'settings.ids' : 'settings.account');
+    }
+
+    function gotoMain() {
+        $state.go('main');
+    }
+
+    MobbrUser[is_id ? 'confirmEmailId' : 'confirmEmail'](params, gotoIds, gotoMain);
 });
