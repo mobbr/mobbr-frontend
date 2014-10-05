b855a75aba8b019d34fb987b50a907adcc385378
Han Dijk <info@handijk.nl>
2014-10-05 16:49:24 +0200

    #97, show correct metadata when host not usable

--- app/scripts/controllers/TaskController.js
+++ app/scripts/controllers/TaskController.js
@@ -6,7 +6,7 @@ angular.module('mobbr.controllers').controller('TaskController', function ($scop
             $state.go('box.task.index.domain');
         } else if (!$scope.has_script) {
             $state.go('box.task.index.script');
-        } if (!$scope.has_participants) {
+        } else if (!$scope.has_participants) {
             $state.go('box.task.index.invite');
         } else if (!$scope.has_payments && $state.is('box.task.index.payments')) {
             $state.go('box.task.index.view');
