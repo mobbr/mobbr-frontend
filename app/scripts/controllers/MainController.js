5647454d87c139bb88ed3495cc900b31fa9baec0
Han Dijk <info@handijk.nl>
2014-10-06 17:26:05 +0200

    #11, add the happening on mobbr right now section

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -1,5 +1,31 @@
-angular.module('mobbr.controllers').controller('MainController', function ($scope, MobbrUri) {
+angular.module('mobbr.controllers').controller('MainController', function ($scope, MobbrApi) {
     'use strict';
+
+    function shuffle(o){ //v1.0
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    };
+
+    $scope.happening = MobbrApi.happening();
+
+    $scope.happening.$promise.then(function (response) {
+
+        $scope.tags = [];
+
+        angular.forEach(response.result.keywords, function (item) {
+            $scope.tags.push({ type: 'keyword', keyword: item.keyword });
+        });
+
+        angular.forEach(response.result.payments, function (item) {
+            $scope.tags.push({ type: 'payment', amount: item.amount, currency_iso: item.currency_iso });
+        });
+
+        angular.forEach(response.result.persons, function (item) {
+            $scope.tags.push({ type: 'person', gravatar: item.gravatar });
+        });
+
+        shuffle($scope.tags);
+    });
 
 //    MobbrUri.get({limit: 4}).$promise.then(function(response){
 //        $scope.featuredTasks = response.result;
