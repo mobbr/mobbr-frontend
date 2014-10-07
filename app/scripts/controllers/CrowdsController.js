a346aaff2a2fe9dacf53c61b25be346eaa950190
Han Dijk <info@handijk.nl>
2014-10-07 10:08:31 +0200

    #169, show more button on crowds en tasks

--- app/scripts/controllers/CrowdsController.js
+++ app/scripts/controllers/CrowdsController.js
@@ -1,11 +1,13 @@
 angular.module('mobbr.controllers').controller('CrowdsController', function ($scope, $state, $window, $rootScope, mobbrMsg, mobbrSession, MobbrUri, MobbrPerson, MobbrKeywords) {
     'use strict';
 
-    var language;
+    var language,
+        initial_limit = 20;
 
-    function queryPeople() {
+    $scope.queryPeople = function () {
 
-        var tags;
+        var tags,
+            params;
 
         if ($scope.filteredTags.length === 0) {
             tags = [];
@@ -16,20 +18,30 @@ angular.module('mobbr.controllers').controller('CrowdsController', function ($sc
             tags = $scope.filteredTags;
         }
 
-        $scope.persons = MobbrPerson.get({
+        params = {
             keywords: tags,
-            language: language
-        });
+            language: language,
+            limit: initial_limit
+        };
 
-        $scope.persons.$promise.then(function () {
+        if ($scope.limiter > initial_limit) {
+            params.offset = $scope.limiter - initial_limit;
+        }
+
+        $scope.personPromise = MobbrPerson.get(params);
+
+        $scope.personPromise.$promise.then(function () {
+
             angular.forEach($scope.selectedPersons, function (selectedPerson) {
-                for (var i = 0; i < $scope.persons.result.length; i++) {
-                    var person = $scope.persons.result[i];
+                for (var i = 0; i < $scope.personPromise.result.length; i++) {
+                    var person = $scope.personPromise.result[i];
                     if (selectedPerson.username === person.username) {
-                        $scope.persons.result[i].selected = true;
+                        $scope.personPromise.result[i].selected = true;
                     }
                 }
             });
+
+            $scope.persons = $scope.persons.concat($scope.personPromise.result);
         });
     }
 
@@ -68,9 +80,9 @@ angular.module('mobbr.controllers').controller('CrowdsController', function ($sc
             });
 
             if ($scope.suggestedTags.length > 0) {
-                queryPeople();
+                $scope.queryPeople();
             } else {
-                getGlobalTags().$promise.then(queryPeople);
+                getGlobalTags().$promise.then($scope.queryPeople);
             }
         } else {
 
@@ -82,7 +94,7 @@ angular.module('mobbr.controllers').controller('CrowdsController', function ($sc
                 });
             }
 
-            queryPeople();
+            $scope.queryPeople();
         }
     }
 
@@ -106,8 +118,7 @@ angular.module('mobbr.controllers').controller('CrowdsController', function ($sc
                 }
             }
         } else {
-            getGlobalTags();
-            queryPeople();
+            getGlobalTags().$promise.then($scope.queryPeople);
         }
     }
 
@@ -151,7 +162,8 @@ angular.module('mobbr.controllers').controller('CrowdsController', function ($sc
             $scope.$emit('set-active-query');
             $scope.$emit('set-query');
             $scope.task = undefined;
-            $scope.persons = undefined;
+            $scope.personPromise = undefined;
+            $scope.persons = [];
             $scope.filteredTags = [];
             $scope.suggestedTags = [];
         }
@@ -167,17 +179,19 @@ angular.module('mobbr.controllers').controller('CrowdsController', function ($sc
         if (new_language !== language) {
             language = new_language;
             $scope.getSuggestedTags();
-            queryPeople();
+            $scope.queryPeople();
         }
     }, true);
 
     $scope.$watch('filteredTags', function (newValue, oldValue) {
         if (newValue && newValue !== oldValue) {
             $scope.getSuggestedTags();
-            queryPeople();
+            $scope.queryPeople();
         }
     }, true);
 
+    $scope.persons = [];
+    $scope.limiter = initial_limit;
     $scope.suggestedTags = [];
     $scope.filteredTags = [];
     $scope.tagsLimiter = { limit: 10 };
