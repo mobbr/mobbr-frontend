96788fb935db57f41e3dc8959ae3016dcc2fa3ac
Han Dijk <info@handijk.nl>
2014-11-13 10:41:39 +0100

    #253, do not send password_control to api and make sure the form is not submitted when the passwords do not match

--- app/scripts/controllers/JoinController.js
+++ app/scripts/controllers/JoinController.js
@@ -1,28 +1,31 @@
 'use strict';
 
-angular.module('mobbr.controllers').controller('JoinController', function ($scope, $stateParams, $window, MobbrUser) {
+angular.module('mobbr.controllers').controller('JoinController', function ($scope, $stateParams, $window, MobbrUser, mobbrMsg) {
 
     $scope.waiting = false;
     $scope.email = $stateParams.email;
     $scope.registerUser = function () {
+        if (!$scope.register.$invalid) {
 
-        $scope.waiting = true;
+            $scope.waiting = true;
 
-        MobbrUser.register({
-            username: $scope.username,
-            email: $scope.email,
-            password: $scope.password,
-            password_control: $scope.password_control
-        }, function (response) {
-            $window.ga('send', 'event', 'account', 'register', 'username', $scope.username);
-            $scope.waiting = false;
-            $scope.email = '';
-            $scope.username = '';
-            $scope.password = '';
-            $scope.password_control = '';
-        }, function (response) {
-            $window.ga('send', 'event', 'error', 'register', 'username', $scope.username);
-            $scope.waiting = false;
-        });
+            MobbrUser.register({
+                username: $scope.username,
+                email: $scope.email,
+                password: $scope.password
+            }, function (response) {
+                $window.ga('send', 'event', 'account', 'register', 'username', $scope.username);
+                $scope.waiting = false;
+                $scope.email = '';
+                $scope.username = '';
+                $scope.password = '';
+                $scope.password_control = '';
+            }, function (response) {
+                $window.ga('send', 'event', 'error', 'register', 'username', $scope.username);
+                $scope.waiting = false;
+            });
+        } else {
+            mobbrMsg.add({ msg: 'Make sure the passwords match', type: 'danger' });
+        }
     }
 });
