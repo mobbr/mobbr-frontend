dd8364848b0b4a8d7a6d46bd908c1dcf00e8195b
Han Dijk <info@handijk.nl>
2014-12-05 10:57:26 +0100

    #271, make invite dependend of "num_recipients" and the number of unique participants

--- app/scripts/controllers/TaskController.js
+++ app/scripts/controllers/TaskController.js
@@ -1,25 +1,20 @@
-angular.module('mobbr.controllers').controller('TaskController', function ($scope, $state, $window, $rootScope, MobbrUri, task) {
+angular.module('mobbr.controllers').controller('TaskController', function ($scope, $state, $window, $rootScope, uniqueFilter, MobbrUri, task) {
     'use strict';
 
-    var url = $window.atob($state.params.task);
+    var url = $state.params.task && $window.atob($state.params.task) || null;
 
-    if (task !== null) {
-
-        if (task.result.script && task.result.script.url && task.result.script.url !== url) {
-            $state.go('box.task', { task: $window.btoa(task.result.script.url) });
-        } else {
-            $scope.task = task;
-            $scope.domain = purl(url).data.attr.host;
-            $scope.url = url;
-            $scope.has_failed = task.result.script && task.result.script.error || false;
-            $scope.has_script = $scope.has_failed && false || task.result.script && task.result.script.url && true;
-            $scope.has_payments = parseFloat(task.result.statistics.num_payments) > 0;
-            $scope.has_participants = task.result.script.participants && task.result.script.participants.length > 0;
-            $scope.$emit('set-query', url);
-            $scope.$emit('set-active-query', url);
-            $scope.$emit('set-task-type', task.result.script.type);
-            $scope.$emit('set-task-message', task.result.script.message);
-            $scope.$emit('set-task-addresses', task.result.addresses);
-        }
+    if (task !== null && task.result.script && task.result.script.url && task.result.script.url !== url) {
+        $state.go('box.task', { task: $window.btoa(task.result.script.url) });
+    } else {
+        $scope.task = task;
+        $scope.domain = purl(url).data.attr.host;
+        $scope.url = url;
+        $scope.has_failed = task.result.script && task.result.script.error || false;
+        $scope.has_script = $scope.has_failed && false || task.result.script && task.result.script.url && true;
+        $scope.has_payments = parseFloat(task.result.statistics.num_payments) > 0;
+        $scope.has_participants = task.result.statistics.num_recipients && task.result.statistics.num_recipients.length > 0 && uniqueFilter(task.result.script.participants, 'id').length > 1;
+        $scope.$emit('set-query', url);
+        $scope.$emit('set-active-query', url);
+        $scope.$emit('set-task', $scope.has_script && task || null);
     }
 });
