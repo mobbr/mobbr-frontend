9e508f7fee3e5435be69174bdea66f3fe8625559
Jason Jackson <jrjackso@gmail.com>
2015-07-25 19:12:02 -0400

    support for other base currencies #98

--- app/scripts/controllers/ForExController.js
+++ app/scripts/controllers/ForExController.js
@@ -2,21 +2,23 @@
 
 angular.module('mobbr.controllers').controller('ForExController', function ($scope, MobbrApi, $state) {
 
-	MobbrApi.currencies().$promise.then(function (response) {
+	var base_currency_iso = $state.params.base_currency_iso.toUpperCase();
+	$scope.base_currency_iso = base_currency_iso;
+
+	MobbrApi.currencies( {base_currency: base_currency_iso} ).$promise.then(function (response) {
 		var result = response.result;
 		var currencies = [];
 		for(var i = 0; i < result.length; i++)
 		{
-			if(result[i].base_currency_iso.toUpperCase() == $state.params.base_currency_iso.toUpperCase())
+			if(result[i].base_currency_iso.toUpperCase() == base_currency_iso)
 			{
 				currencies.push(result[i]);
 			}
 		}
 		$scope.currencies = currencies;
-		if(currencies.length <= 0)
+		if(currencies.length == 0)
 			return;
 		
-		$scope.base_currency_iso = result[0].base_currency_iso;
 		$scope.exchange_rate_datetime = result[0].exchange_rate_datetime;
 	});
 });
